e51493c21a719d226bf770a4412f6cdf54324d5a
alx652 <welscha2@nih.gov>
2026-02-06 09:59:28 -0500

    springBoot3x and roles

--- gsrs-module-adverse-events-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/adverseevents/adverseeventpt/models/AdverseEventFAERSDashboard.java
+++ gsrs-module-adverse-events-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/adverseevents/adverseeventpt/models/AdverseEventFAERSDashboard.java
@@ -28,7 +28,7 @@ import gsrs.springUtils.AutowireHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 import java.util.Date;
 import java.util.List;
